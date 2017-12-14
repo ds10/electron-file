9d8c9092e2d38892dd1dc4afedc529f0c93fd5c8
David Sherlock <davidsherlock163@hotmail.com>
2017-12-14 11:52:17 +0000

    inital commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const {app, BrowserWindow, dialog} = require('electron')
+const fs = require('fs');
+
+const url = require('url')
+const path = require('path')
+
+app.on('ready', createWindow)
+
+let win
+
+function createWindow () {
+    // Create the browser window.
+    win = new BrowserWindow({width: 800, height: 600})
+  
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+  
+    // Open the DevTools if you want to expect the page
+    //win.webContents.openDevTools()
+  
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      win = null
+    })
+  }
